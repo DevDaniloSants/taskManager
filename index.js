d4bf49bd08795e790452f8114fb0d1e2292acde1
Danilo Santos <dscofield@outlook.com.br>
2024-03-13 21:42:51 -0300

    feat: add router file to task entity

--- index.js
+++ index.js
@@ -1,83 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const taskRouter = require("./src/routes/task.routes");
 const connectToDatabase = require("./src/database/mongoose.database");
-const TaskModel = require("./src/models/task.model.js");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use("/tasks", taskRouter);
 connectToDatabase();
-
-app.get("/tasks", async (req, res) => {
-    try {
-        const tasks = await TaskModel.find({});
-        res.status(201).send(tasks);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await TaskModel.findById(taskId);
-
-        if (!task) return res.status(404).send("Tarefa não encontrada");
-
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.post("/tasks", async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-        await newTask.save();
-
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.put("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = req.body;
-
-        const taskToUpdate = await TaskModel.findByIdAndUpdate(
-            taskId,
-            taskData,
-            {
-                new: true,
-            }
-        );
-
-        res.status(200).send(taskToUpdate);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-
-        const taskToDelete = await TaskModel.findById(taskId);
-
-        if (!taskToDelete) return res.status(404).send("Tarefa não encontrada");
-
-        const deleteTask = await TaskModel.findByIdAndDelete(taskToDelete);
-
-        res.status(201).send(deleteTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
 
 app.listen(8000, () => {
     console.log("Listening on port 8000!");
